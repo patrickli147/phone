aa5284c974a176c4ac83edf5faab95930d76c58c
patrickli147 <49908827+patrickli147@users.noreply.github.com>
2021-10-09 18:48:12 +0800

    feat: add drag component

---
+++ src/components/common/draggable/DraggableItem.vue
@@ -0,0 +1,47 @@
+<template>
+    <div
+        @dragstart="onDragStart"
+        @drag="onDrag"
+        @dragend="onDragEnd"
+        @drop="onDrop(e)"
+        @dragenter="onDragEnter"
+        @dragover="onDragOver"
+        @dragleave="onDragLeave"
+        draggable="true"
+    >
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'draggable-item',
+    methods: {
+        onDragStart() {
+            console.log('drag start');
+        },
+        onDrag() {
+
+        },
+        onDragEnd() {
+            console.log('drag end');
+        },
+        onDrop(e) {
+            e.stopPropagation();
+        },
+        onDragEnter(e) {
+            e.stopPropagation();
+        },
+        onDragOver(e) {
+            e.stopPropagation();
+        },
+        onDragLeave(e) {
+            e.stopPropagation();
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/common/draggable/DropZone.vue
@@ -0,0 +1,44 @@
+<template>
+    <div
+        class="container"
+        @drop="onDrop"
+        @dragenter="onDragEnter"
+        @dragover="onDragOver"
+        @dragleave="onDragLeave"
+    >
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+// import DraggableItem from './DraggableItem.vue';
+
+export default {
+    name: 'drop-zone',
+    methods: {
+        onDrop(e) {
+            // 取消默认拖拽
+            e.preventDefault();
+
+            console.log('drop');
+        },
+        onDragEnter() {
+            console.log('drag enter');
+        },
+        onDragOver(e) {
+            // console.log('drag over');
+            e.preventDefault();
+        },
+        onDragLeave() {
+            console.log('drag leave');
+        }
+    },
+    components: {
+
+    }
+}
+</script>
+
+<style>
+
+</style>
